0775869f1058b0a5ed12ee17ca54967e53503f32
Mike Watkins <solutionroute@gmail.com>
2018-02-18 04:23:34 -0800

    working through issues

--- configure-new-host.sh
+++ configure-new-host.sh
@@ -10,20 +10,19 @@
 
 # Add some build tools needed by various apps including VBox
 
+# TODO - add stretch-backports main contribu  - to /etc/apt/sources.list
+
 # VBox pre-requisites
 sudo apt-get install -y build-essential module-assistant
 sudo m-a prepare
 sudo apt-get install -y -t stretch-backports virtualbox-guest-dkms virtualbox-guest-x11 linux-headers-$(uname -r)
-read -p "Press any key to continue"
-sudo mount /dev/cdrom /media/cdrom && cd /dev/cdrom
-sudo ./VBoxLinuxAdditions.run
-cd ~
 
 # tools
 sudo apt-get install -y git
 
 # XOrg
 sudo apt-get install -y xorg
+sudo apt-get install -y fontconfig pkgconfig libfreetype6 libfreetype6-dev xorg-dev
 sudo apt-get install -y dwm
 mkdir -p ~/src
 cd ~/src
@@ -31,6 +30,9 @@ git clone git://git.suckless.org/st
 cd st
 make
 sudo make install
+cd ~/src
+# grab the source for later customization
+apt-get source dwm
 
 # TODO test for existance of .xinitrc otherwise create it
 
@@ -43,3 +45,12 @@ sudo make install
 #
 #
 # add Go / httphere / etc
+
+
+# Finish off VirtualBox
+sudo mount /dev/cdrom /media/cdrom 
+cd /dev/cdrom
+read -p "If the following command does not complete, check the end of this script and investigate /media/cdrom - is the ISO mounted?"
+sudo ./VBoxLinuxAdditions.run
+cd ~
+echo "\nDone!"
